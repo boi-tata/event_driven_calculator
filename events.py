02f65616bf3c9b038397ae5ed1743b4556b68c04
Luiz Fernando <luizf.prodigio@gmail.com>
2020-06-01 14:26:07 -0300

    Módulo de eventos criado

---
+++ events.py
@@ -0,0 +1,66 @@
+"""
+Módulo que contém todos os elementos relacionados a eventos.
+"""
+
+class Event:
+    """
+    Classe base para todos os eventos da aplicação.
+    """
+
+    pass
+
+
+class InputEvent(Event):
+    """
+    Input de usuário.
+    """
+
+    __slots__ = ('key',)
+
+    def __init__(self, key):
+        self.key = key
+
+
+class OperationEvent(Event):
+    """
+    Operação a ser realizada.
+    """
+
+    __slots__ = ('operator', 'operands')
+
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+
+class ResultEvent(Event):
+    """
+    Resultado de uma operação realizada.
+    """
+
+    __slots__ = ('result',)
+
+    def __init__(self, result):
+        self.result = result
+
+
+class ErrorEvent(Event):
+    """
+    Erro ocorrido durante processamento de um evento.
+    """
+
+    __slots__ = ('source', 'error', 'type', 'description')
+
+    def __init__(self, source_event:Event, error:Exception):
+        if not isinstance(source_event, Event):
+            raise TypeError("'source_event' needs to be a Event instance.")
+
+        if not isinstance(error, Exception):
+            raise TypeError("'error' needs to be a Exception instance.")
+
+        self.source = source_event
+        self.error = error
+        self.type = error.__class__.__name__
+        self.description = str(error)
+
+
